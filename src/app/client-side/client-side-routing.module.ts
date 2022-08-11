e19a235b9cf46746ee015cba34b743e6abc084b4
racha <rachabnabid@gmail.com>
2022-08-11 02:20:06 +0100

    navbar

--- src/app/client-side/client-side-routing.module.ts
+++ src/app/client-side/client-side-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientSideComponent } from './client-side.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [{ path: '', component: ClientSideComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '', component: ClientSideComponent,
+    children: [
+      {
+        path:'home', component: HomeComponent
+      },
+      {
+        path:'events', component: EventListComponent
+      },
+      {
+        path:'events-details', component: EventDetailsComponent
+      },
+      {
+        path:'reservation', component: ReservationComponent
+      }
+    ]
+  },
+  {
+    path: 'navbar', component: NavbarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
